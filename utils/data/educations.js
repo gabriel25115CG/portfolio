952fb66257b8fbd369952eebed18f1965f078c9b
unknown <gabriel.chalmet@utbm.fr>
2025-06-03 14:43:42 +0200

    First

---
+++ utils/data/educations.js
@@ -0,0 +1,14 @@
+export const educations = [
+  {
+    id: 1,
+    title: "Engineering Degree - Information Systems Engineering, Network & Cybersecurity",
+    duration: "2022 - Present",
+    institution: "UTBM (Université de Technologie de Belfort-Montbéliard)",
+  },
+  {
+    id: 2,
+    title: "BTS SIO - Option SISR (IT Infrastructure Solutions)",
+    duration: "2020 - 2022",
+    institution: "Lycée Louis Pergaud, Besançon",
+  }
+];
---
+++ utils/data/experience.js
@@ -0,0 +1,14 @@
+export const experiences = [
+  {
+    id: 1,
+    title: "Cybersecurity Engineer Apprentice",
+    company: "Flowbird",
+    duration: "(2022 - Present)"
+  },
+  {
+    id: 2,
+    title: "Network and Cybersecurity Administrator Apprentice",
+    company: "Abéo Group",
+    duration: "(2021 - 2022)"
+  }
+];
