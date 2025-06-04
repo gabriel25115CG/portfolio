0b60cc146aa12843dd63467e12bcc700760be2cf
unknown <gabriel.chalmet@utbm.fr>
2025-06-04 09:29:57 +0200

    Details

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,17 +1,17 @@
 export const personalData = {
   name: "Gabriel CHALMET",
-  profile: '/profile.png',
+  profile: '/profile4.png',
   designation: "Cybersecurity & System Engineer",
   description: "My name is Gabriel CHALMET. I am a professional and enthusiastic cybersecurity specialist in my daily life. I am a quick learner with a strong self-learning attitude. I am passionate about exploring new security technologies and solving complex cybersecurity challenges. I have a deep interest in all aspects of information security, from securing web applications to analyzing vulnerabilities and strengthening system defenses. My core skills are rooted in JavaScript, and I enjoy using it for developing secure solutions and detecting potential threats in web environments. I am available for any cybersecurity-related job opportunity that aligns with my skills and interests.",
   email: 'gabriel.chalmet25115@gmail.com',
   phone: '+33 06 09 28 07 05',
   address: 'Somewhere in Besançon',
   github: 'https://github.com/Gabriel25115CG',
-  facebook: 'https://www.facebook.com/abusaid.riyaz/',
-  linkedIn: 'https://www.linkedin.com/in/abu-said-bd/',
-  twitter: 'https://twitter.com/said7388',
-  stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
+  facebook: 'https://www.facebook.com/',
+  linkedIn: 'https://www.linkedin.com/in/gabriel-chalmet-a5540220b/',
+  twitter: 'https://twitter.com/',
+  stackOverflow: 'https://stackoverflow.com/',
   leetcode: "https://leetcode.com/said3812/",
-  devUsername: "said7388",
+  devUsername: "Gabriel0day",
   resume: "https://drive.google.com/file/d/1eyutpKFFhJ9X-qpQGKhUNnVRkB5Wer00/view?usp=sharing"
 }
